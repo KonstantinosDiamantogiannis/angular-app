7b34a9a126c7250a572596223ca4241f19d85119
Kostas Diam <tsingoku@yahoo.gr>
2022-03-29 20:06:45 +0300

    Using attr in property binding(Attribute Binding)

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,13 +4,12 @@ import { CoursesService } from "./app/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{title}}</h2>
-    <h2 [textContent]="title"></h2>
-    <img src="{{imageUrl}}"> 
-    <img [src]="imageUrl">`
+    template: `<table>
+        <tr>
+            <td [attr.colspan]="colSpan">Cell 1</td>
+        </tr>
+    </table>`
 })
 export class CoursesComponent {
-    title = "List of Courses";
-
-    imageUrl = "https://external-preview.redd.it/H_kdqE_Vf7_z7nx2yJvCJy1bBGEbyKNtiATMuME72-E.jpg?auto=webp&s=973916abfe281d34ee80733481b200399d3f2b8e";
+    colSpan = 2;
 }
