934af7e1d78ac03878b18913f0f13f4a78c0937d
Kostas Diam <tsingoku@yahoo.gr>
2022-03-29 19:55:57 +0300

    Interpolation vs Property Binding

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,18 +4,13 @@ import { CoursesService } from "./app/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `<h2>{{"Title: " + title}}</h2> 
-    <ul>
-        <li *ngFor="let course of courses">{{course}}</li>
-    </ul>`
+    template: `<h2>{{title}}</h2>
+    <h2 [textContent]="title"></h2>
+    <img src="{{imageUrl}}"> 
+    <img [src]="imageUrl">`
 })
 export class CoursesComponent {
     title = "List of Courses";
 
-    courses: any[] = [];
-
-    constructor(service: CoursesService){
-        // let service = new CoursesService();
-        this.courses = service.getCourses();
-    }
+    imageUrl = "https://external-preview.redd.it/H_kdqE_Vf7_z7nx2yJvCJy1bBGEbyKNtiATMuME72-E.jpg?auto=webp&s=973916abfe281d34ee80733481b200399d3f2b8e";
 }
