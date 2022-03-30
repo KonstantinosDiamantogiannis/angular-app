02e920bafb767db9ea33859f230b9524bbcad023
Kostas Diam <tsingoku@yahoo.gr>
2022-03-30 18:49:08 +0300

    Custom Pipes(Dynamic summary limit)

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,7 +4,7 @@ import { CoursesService } from "./app/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `{{text | summary }}`
+    template: `{{text | summary : 10 }}`
 })
 export class CoursesComponent {
     text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit impedit commodi beatae alias mollitia illo, 
