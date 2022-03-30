c7cc08f0cee7c049c3dabaf28f1f3af0fde5a3b9
Kostas Diam <tsingoku@yahoo.gr>
2022-03-30 18:27:03 +0300

    Pipes Intro

--- src/courses.component.ts
+++ src/courses.component.ts
@@ -4,13 +4,18 @@ import { CoursesService } from "./app/courses.service";
 
 @Component({
     selector: 'courses',
-    template: `
-    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()"/>`
+    template: `{{course.title | uppercase}} <br>
+    {{course.students | number}} <br> 
+    {{course.rating | number: '2.2-3'}} <br>
+    {{course.price | currency: 'EUR':'symbol' }} <br>
+    {{course.releaseDate | date: 'shortDate'}}`  // kai sketo date einai: May, 2, 2016
 })
 export class CoursesComponent {
-    email = "me@mail.com";
-    onKeyUp(){
-        console.log(this.email);        
+    course = {
+        title: "The complete Angular course",
+        students: 30123,
+        rating: 4.9745,
+        price: 190.95,
+        releaseDate: new Date(2016,2,3)
     }
-
 }
